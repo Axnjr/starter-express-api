4e4a4f24b4691c1fd629b149a1d0464e43546182
Yakshit <yakshitchhipa@gmail.com>
2024-07-08 08:10:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 2989, () => {
     console.log("RUNNING ON PORT: 3000");
 })
